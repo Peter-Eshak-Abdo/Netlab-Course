55625eb37861161e8342760fa7b9a039473a8227
Peter Eshak Abdo <145208705+Peter-Eshak-Abdo@users.noreply.github.com>
2024-10-20 14:54:45 +0000

    task14

--- js/main.js
+++ js/main.js
@@ -2,3 +2,33 @@ function myFunction() {
   var element = document.body;
   element.dataset.bsTheme = element.dataset.bsTheme == "light" ? "dark" : "light";
 }
+
+/* Start Draft index.html */
+/* const pos = document.getElementById("ip");
+function getGeolocation() {
+  if (navigator.geolocation) {
+     navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else {
+    pos.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+function showPosition(position) {
+  pos.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+}
+function showError(error) {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      x.innerHTML = "User denied the request for Geolocation.";
+      break;
+    case error.POSITION_UNAVAILABLE:
+      x.innerHTML = "Location information is unavailable.";
+      break;
+    case error.TIMEOUT:
+      x.innerHTML = "The request to get user location timed out.";
+      break;
+    case error.UNKNOWN_ERROR:
+      x.innerHTML = "An unknown error occurred.";
+      break;
+  }
+}*/
+/* End Draft index.html */
