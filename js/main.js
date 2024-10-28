e211dc35369adf523a595bdbb238a8d3b76ad48c
Peter Eshak Abdo <145208705+Peter-Eshak-Abdo@users.noreply.github.com>
2024-10-28 19:47:36 +0000

    make a HTML file without any single HTML Code just JavaScript.

--- js/main.js
+++ js/main.js
@@ -32,4 +32,46 @@ function showError(error) {
       break;
   }
 }*/
+let names = ["Peter", "Eshak", "Abdo", "Gabra"];
+let ages = ["19 years old","56 years old","62 years old","59 years old"]
+
+
+let container = document.createElement("div");
+document.body.appendChild(container);
+container.style.textAlign = "center";
+
+function element(names,ages) {
+  // Element
+  let card = document.createElement("div");
+  let title = document.createElement("h2");
+  let age = document.createElement("p");
+  let img = document.createElement("img");
+
+  // Content
+  let head = document.createTextNode(names);
+  let ageContent = document.createTextNode(ages);
+  img.src = "../../images/face.jpg";
+  img.alt = "Error src of image";
+  title.appendChild(head);
+  age.appendChild(ageContent);
+
+  // Style
+  card.style.width = "200px";
+  card.style.background = "#444";
+  card.style.color = "#fff";
+  card.style.padding = "10px";
+  card.style.margin = "3px";
+  card.style.display = "inline-block";
+  img.style.width = "100%";
+
+  card.appendChild(title);
+  card.appendChild(age);
+  card.appendChild(img);
+  container.appendChild(card);
+}
+for (let i = 0; i < 4; i++) {
+  element(names[i],ages[i]);
+}
+
+
 /* End Draft index.html */
