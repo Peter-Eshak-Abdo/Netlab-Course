aca5c8baeea08e01185b08d2178f23c1759582a2
Peter Eshak Abdo <145208705+Peter-Eshak-Abdo@users.noreply.github.com>
2024-11-12 22:53:18 +0200

    lesson 21 & Task 18 First Lesson in JavaScript

---
+++ sites/tasksApp/task18/main.js
@@ -0,0 +1,12 @@
+var fName = "Peter Eshak";
+var fullName = "Peter Eshak Abdo Gabra";
+var age = 19;
+var country = "Ismaillia"
+var phoneNumber = "01221331602/01202224608"
+
+document.getElementById("name").innerText = fName;
+document.getElementById("fName").innerText = fullName;
+document.getElementById("age").innerHTML = age;
+document.getElementById("phoneNum").innerText = phoneNumber;
+document.getElementById("country").innerHTML = country;
+
